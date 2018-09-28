db58c08125128fbad9172c15539e86c3bc127659
piratebernard <bernardsteemers@Nib.local>
2018-09-28 14:06:18 +0100

    ACII to Decimal

---
+++ main 15.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Lab1Q16
+//
+//  Created by Bernard Steemers on 28/09/2018.
+//  Copyright © 2018 Bernard Steemers. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char x;
+    cout << "Enter 0-9";
+    cin >> x;
+    cout << (int)x;
+    
+}
